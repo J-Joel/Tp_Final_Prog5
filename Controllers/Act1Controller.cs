9cf00603982e038cff80d9b4f80fac0a1a9b75c2
Joel <jyucharico@gmail.com>
2025-03-28 09:51:12 -0300

    Actividad 2
    Actividad relacianada a los Controladores

---
+++ Controllers/Act1Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP_Final_Programacion5.Controllers
+{
+    public class Act1Controller : Controller
+    {
+        /* Se supone que no deberia de especificar siempre el controlador a usar, solamente para cambiar, pero hay que siempre especificarlo */
+        /******************************************************/
+        /* Los controladores tienen una funcion especial, esta funcionalidad es la de trabajar con el nombre del controlador
+         * [Nombre de controlador]Controller, la acciones del controlador buscan en la carpeta de vistas el [Nombre de controlador]
+         * automaticamente, esto quiere decir que accederan a los htmls directamente a esa carpeta */
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+        public IActionResult Privado()
+        {
+            return View();
+        }
+        /******************************************************/
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,22 +19,6 @@ namespace TP_Final_Programacion5.Controllers
             // Devuelve la vista/plantilla segun como se llame la funcion interface(En este caso index),
             // si no existe una vista de igual nombre, tira error al no encontrar el csHTML
         }
-
-        /******************************************************/
-        public IActionResult Act1()
-        {
-            return View("../Act1/Act1");
-        }
-        public IActionResult Contacto1()
-        {
-            return View("../Act1/Contacto1");
-        }
-        public IActionResult Privado1()
-        {
-            return View("../Act1/Privado1");
-        }
-        /******************************************************/
-
         // Funcion interface que informa los errores al momento de cargar una vista
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
